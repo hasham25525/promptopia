5f4eee338014c9488f4f658a960d9b05376d5c74
Muhammad Hasham <hasham25525@gmail.com>
2023-08-03 03:41:40 +0500

    errors fixed

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import { Schema, model, models } from "mongoose";
-const UserSchema =  newSchema({
+const UserSchema =  new Schema({
     email: {
         type: String,
         required: [true, "Email is required!"],
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require('mongoose')
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
