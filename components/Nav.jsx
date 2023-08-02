e85d78111f5c938553b0e9537f0c6e92f6496695
Muhammad Hasham <hasham25525@gmail.com>
2023-08-03 04:04:52 +0500

    Google Auth working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggeleDropdown, setToggeleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response)
     }
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -36,7 +37,7 @@ flex- center">
 
       {/* Desktop nav  */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
               className="black_btn">
@@ -49,7 +50,7 @@ flex- center">
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -79,10 +80,10 @@ flex- center">
 
       {/* Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
