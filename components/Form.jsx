25c0eca30e88c1db8c2dce8fa5c38998752c8592
Muhammad Hasham <hasham25525@gmail.com>
2023-08-07 01:04:29 +0500

    create prompt form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import Link from "next/link"
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit, }) => {
+
+
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing promts with the world, and let your imagination run wild with any AI-powerd plateform.
+        Please fill in the form below to create a new blog.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-full flex flex-col gap-7 glassmorphism"
+      >
+        {/* textarea */}
+        <label >
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI prompt
+          </span>
+
+          <textarea
+            placeholder="Write your prompt here..."
+            value={post.prompt}
+            required
+            className="form_textarea"
+            onChange={(e) => setPost({
+              ...post, prompt: e.target.value
+            })}
+          />
+        </label>
+
+        {/* tag */}
+        <label >
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag{` `}
+            <span className="font normal"> 
+            (#product, #webdevelopement, #idea)
+            </span>
+          </span>
+
+          <input
+            placeholder="#tag"
+            value={post.tag}
+            required
+            className="form_input"
+            onChange={(e) => setPost({
+              ...post, tag: e.target.value
+            })}
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+            <Link href='/' className="text-gray-500 text-sm">
+              Cancel
+            </Link>
+        </div>
+      </form>
+
+    </section>
   )
 }
 
