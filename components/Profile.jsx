6385d421496182fae59a96b8ea5f7b824db05d72
Muhammad Hasham <hasham25525@gmail.com>
2023-08-08 03:36:43 +0500

    minor changes

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -13,16 +13,15 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
           <PromptCard
             key={post._id}
             post={post}
-            // handleEdit={() => handleEdit && handleEdit(post)}
-            // handleDelete={() => handleDelete && handleDelete(post)}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
           />
         ))
         }
-
       </div>
 
     </section>
   )
 }
 
-export default Profile
+export default Profile;
