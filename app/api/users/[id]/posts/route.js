d2dfec73fbb6d1189c100ae829c0f1431c0f7291
Muhammad Hasham <hasham25525@gmail.com>
2023-08-08 02:20:30 +0500

    profile page

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,17 @@
+import { connectDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectDB();
+        const prompts = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { status: 200 })
+
+    } catch (error) {
+        return (
+            new Response("failed to fatch all posts", { status: 500 }))
+    }
+}
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,19 +5,38 @@ import { useRouter } from "next/navigation"
 
 import Profile from "@components/Profile"
 
-const handleEdit = () => {
-
-}
-const handleDelete = async () => {
-
-}
 
 const MyProfile = () => {
+
+  const { data: session } = useSession;
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+
+  const handleEdit = () => {
+
+  }
+  const handleDelete = async () => {
+
+  }
+
+
   return (
     <Profile
       name='My'
       desc="welcome to your personalized profile page"
-      data={[]}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
