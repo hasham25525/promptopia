1bed8850594ea57de170d5ea4f8e6d63af691d8d
Muhammad Hasham <hasham25525@gmail.com>
2023-08-08 01:30:34 +0500

    MyProfile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import Profile from "@components/Profile"
+
+const handleEdit = () => {
+
+}
+const handleDelete = async () => {
+
+}
 
 const MyProfile = () => {
-  return ( 
-    <div>MyProfile</div>
+  return (
+    <Profile
+      name='My'
+      desc="welcome to your personalized profile page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
