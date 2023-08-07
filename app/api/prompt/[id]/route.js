94c22276355751fb4a117010a06f747dabf4bf08
Muhammad Hasham <hasham25525@gmail.com>
2023-08-08 04:28:22 +0500

    GET, PATCH & DELETE

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -17,24 +17,34 @@ export const GET = async (request, { params }) => {
 }
 
 //PATCH (update)
-export const PATCH =async(request, {params})=>{
-    const {prompt,tag}= await request.json();
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json();
     try {
         await connectDB();
 
         const existingPrompt = await Prompt.findById(params.id);
 
-        if(!existingPrompt) return new Response("Prompt not found", {status:404})
+        if (!existingPrompt) return new Response("Prompt not found", { status: 404 })
 
-        existingPrompt.prompt=prompt;
-        existingPrompt.tag=tag;
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
 
         await existingPrompt.save();
 
-        return new Response(JSON.stringify(existingPrompt),{status:200})
+        return new Response(JSON.stringify(existingPrompt), { status: 200 })
     } catch (error) {
-        const new Response("failed to update prompt",{status:500})
+        return new Response("failed to update prompt", { status: 500 })
     }
 }
 
 //DELETE (delete)
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectDB();
+        await Prompt.findByIdAndRemove(params.id);
+        return new Response(` ${params.id} Deleted succesfully`,{status : 200})
+    } catch (error) {
+        return new Response("failed to delete prompt", { status: 500 })
+
+    }
+}
