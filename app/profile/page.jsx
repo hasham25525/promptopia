e39eca9437ce479c14c860b3ebbddd2f79c1c4c6
Muhammad Hasham <hasham25525@gmail.com>
2023-08-09 04:04:34 +0500

    error fixed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 
 import Profile from "@components/Profile"
-import { factory } from "typescript"
+
 
 
 const MyProfile = () => {
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -22,6 +22,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   return (
     <div className="prompt_card">
+      hello
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
