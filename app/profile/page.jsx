227dd5966ad17181c829f5e4bcb55ca7b4c13898
Muhammad Hasham <hasham25525@gmail.com>
2023-08-09 03:58:08 +0500

    delete post functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 
 import Profile from "@components/Profile"
+import { factory } from "typescript"
 
 
 const MyProfile = () => {
@@ -32,9 +33,13 @@ const MyProfile = () => {
     const hasConfirmed = confirm("Are you sure you want to delete this proompt?")
 if(hasConfirmed){
   try {
-     
+     await fatch(`/api/prompt/${post._id.toString()}`,{
+      methode:'DELETE'
+     })
+     const filterPosts= posts.filter((p)=> p._id !== post._id);
+     setPosts(filterPosts)
   } catch (error) {
-    
+    console.log(error);
   }
 }
   }
