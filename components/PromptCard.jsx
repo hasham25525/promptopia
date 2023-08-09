03cad77ad52253a6220ef3a173b1de24be888669
Muhammad Hasham <hasham25525@gmail.com>
2023-08-10 02:08:22 +0500

    username onClick

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { useState } from "react"
 import Image from "next/image"
 import { useSession } from "next-auth/react"
@@ -7,10 +8,19 @@ import { usePathname, useRouter } from "next/navigation"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const { data: session } = useSession();
-  const pathName = usePathname;
-  const router =useRouter;
+  const pathName = usePathname();
+  const router = useRouter();
 
   const [copy, setCopy] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
 
   const handleCopy = () => {
     setCopy(post.prompt);
@@ -23,7 +33,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
