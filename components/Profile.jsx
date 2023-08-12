4d9b7a0699c5fee4766f9020609e647141a5cf5f
Muhammad Hasham <hasham25525@gmail.com>
2023-08-13 01:56:28 +0500

    conditions in profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -8,16 +8,20 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
       </h1>
       <p className='desc text-left'>{desc}</p>
 
-      <div className='mt-10 prompt_layout'>
-        {data.map((post) => (
-          <PromptCard
-            key={post._id}
-            post={post}
-            handleEdit={() => handleEdit && handleEdit(post)}
-            handleDelete={() => handleDelete && handleDelete(post)}
-          />
-        ))}
-      </div>
+      {
+        data == null
+          ? <div><p className=" ">create prompt</p></div>
+          : <div className='mt-10 prompt_layout'>
+            {data.map((post) => (
+              <PromptCard
+                key={post._id}
+                post={post}
+                handleEdit={() => handleEdit && handleEdit(post)}
+                handleDelete={() => handleDelete && handleDelete(post)}
+              />
+            ))}
+          </div>
+      }
     </section>
   );
 };
