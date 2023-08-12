5983e468f7b0ee0fd270d5c0a8cb932e23c37885
Muhammad Hasham <hasham25525@gmail.com>
2023-08-13 02:23:05 +0500

    changes in Profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,4 +1,5 @@
 import PromptCard from "./PromptCard";
+import Link from "next/link";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   console.log(data);
@@ -10,9 +11,16 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
       <p className='desc text-left'>{desc}</p>
 
       {
-        data.length==0 
-          ? <div className='mt-10 prompt_layout'><p className=" text-primary-orange">No Prompts to show...</p>
-          
+        data.length == 0
+          ? <div className='mt-10 '>
+            <p className=" text-primary-orange">No Prompts to show...</p>
+             <div className="mt-4 ">
+             <Link href="/create-prompt"
+              className="black_btn w-40">
+              Create Prompt ?
+            </Link>
+             </div>
+
           </div>
           : <div className='mt-10 prompt_layout'>
             {data.map((post) => (
