764eebc81454a939e888ad1c3f21c40a0d3804d5
Muhammad Hasham <hasham25525@gmail.com>
2023-08-18 02:58:34 +0500

    date edited

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -46,9 +46,19 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <h3 className="font-satoshi font-semibold text-gray-900">
               {post?.creator?.username}
             </h3>
-            <p className="font-inter text-sm text-gray-500 ">
-              {post?.updatedAt?.slice(0,10)}
-            </p>
+            {
+              post?.updatedAt
+                ? (
+                  <p className="font-inter text-sm text-gray-500 ">
+                    {post?.updatedAt?.slice(0, 10)}
+                  </p>
+                )
+                : (
+                  <p className="font-inter text-sm text-gray-500 ">
+                    {post?.date?.slice(0, 10)}
+                  </p>
+                )
+            }
           </div>
         </div>
 
@@ -71,7 +81,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </p>
 
 
-      {session?.user.id === post.creator?._id && pathName === "/profile" && (
+      {session?.user.id === post?.creator?._id && pathName === "/profile" && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
             className='font-inter text-sm green_gradient cursor-pointer'
