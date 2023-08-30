56a27b9f751c027c9ed55721b9bf769fdbafc3d4
Muhammad Hasham <hasham25525@gmail.com>
2023-08-31 01:12:26 +0500

    prettify js

--- models/prompt.js
+++ models/prompt.js
@@ -1,18 +1,21 @@
 import { Schema, model, models } from "mongoose";
 
-const PromptSchema = new Schema({
+const PromptSchema = new Schema(
+  {
     creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     prompt: {
-        type: String,
-        required: [true, "Please add a prompt"]
+      type: String,
+      required: [true, "Please add a prompt"],
     },
     tag: {
-        type: String,
-        required: [true, "Please add a tag"]
-    }
-}, { timestamps: true });
+      type: String,
+      required: [true, "Please add a tag"],
+    },
+  },
+  { timestamps: true }
+);
 const Prompt = models.Prompt || model("Prompt", PromptSchema);
 export default Prompt;
